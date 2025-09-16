859784e5a21ad2debfc754d2af535b265e7dfa1e
Jean C Miguel <fullstack.jeancmiguel@gmail.com>
2025-09-16 14:27:34 -0300

    Add files via upload

---
+++ DiferencaIdade.java
@@ -0,0 +1,8 @@
+public class DiferencaIdade {
+    public static void main(String[] args){
+        int firstAge = 25;
+        int secondAge = 96;
+        int ageDifference = firstAge - secondAge;
+        System.out.println("the difference is: " + ageDifference);
+    }
+}
